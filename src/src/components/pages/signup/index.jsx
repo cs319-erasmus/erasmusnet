32a3f1b95439ac49bb02551119e52b8e8f60e26d
arda <arda.baktir@ug.bilkent.edu.tr>
2022-12-12 17:04:26 +0300

    new sign up page

--- src/src/components/pages/signup/index.jsx
+++ src/src/components/pages/signup/index.jsx
@@ -1,74 +1,203 @@
-import React from 'react'
+// import React, { useContext } from "react";
+// import styled from "styled-components";
+// import { motion } from "framer-motion";
+// import { Marginer } from "../marginer";
+// import { AccountContext } from "./accountContext";
+// export const BoxContainer = styled.div`
+//   width: 100%;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   margin-top: 10px;
+// `;
+//
+// export const FormContainer = styled.form`
+//   width: 100%;
+//   display: flex;
+//   flex-direction: column;
+//   box-shadow: 0px 0px 2.5px rgba(15, 15, 15, 0.19);
+// `;
+//
+// export const MutedLink = styled.a`
+//   font-size: 11px;
+//   color: rgba(200, 200, 200, 0.8);
+//   font-weight: 500;
+//   text-decoration: none;
+// `;
+//
+// export const BoldLink = styled.a`
+//   font-size: 11px;
+//   color: rgb(241, 196, 15);
+//   font-weight: 500;
+//   text-decoration: none;
+//   margin: 0 4px;
+// `;
+//
+// export const Input = styled.input`
+//   width: 100%;
+//   height: 42px;
+//   outline: none;
+//   border: 1px solid rgba(200, 200, 200, 0.3);
+//   padding: 0px 10px;
+//   border-bottom: 1.4px solid transparent;
+//   transition: all 200ms ease-in-out;
+//   font-size: 12px;
+//   &::placeholder {
+//     color: rgba(200, 200, 200, 1);
+//   }
+//   &:not(:last-of-type) {
+//     border-bottom: 1.5px solid rgba(200, 200, 200, 0.4);
+//   }
+//   &:focus {
+//     outline: none;
+//     border-bottom: 2px solid rgb(241, 196, 15);
+//   }
+// `;
+//
+// export const SubmitButton = styled.button`
+//   width: 100%;
+//   padding: 11px 40%;
+//   color: #fff;
+//   font-size: 15px;
+//   font-weight: 600;
+//   border: none;
+//   border-radius: 100px 100px 100px 100px;
+//   cursor: pointer;
+//   transition: all, 240ms ease-in-out;
+//   background: rgb(241, 196, 15);
+//   background: linear-gradient(
+//     58deg,
+//     rgba(241, 196, 15, 1) 20%,
+//     rgba(243, 172, 18, 1) 100%
+//   );
+//   &:hover {
+//     filter: brightness(1.03);
+//   }
+// `;
+// export function SignupForm(props) {
+//     const { switchToSignin } = useContext(AccountContext);
+//
+//     return (
+//         <BoxContainer>
+//             <FormContainer>
+//                 <Input type="text" placeholder="Full Name" />
+//                 <Input type="email" placeholder="Email" />
+//                 <Input type="password" placeholder="Password" />
+//                 <Input type="password" placeholder="Confirm Password" />
+//             </FormContainer>
+//             <Marginer direction="vertical" margin={10} />
+//             <SubmitButton type="submit">Signup</SubmitButton>
+//             <Marginer direction="vertical" margin="1em" />
+//             <MutedLink href="#">
+//                 Already have an account?
+//                 <BoldLink href="#" onClick={switchToSignin}>
+//                     Signin
+//                 </BoldLink>
+//             </MutedLink>
+//         </BoxContainer>
+//     );
+// }
+import React from 'react';
+import styled from 'styled-components';
+import Logo from "../../common/logo";
+
+const OutsideContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  margin-top: -10rem;
+  @media (min-width: 640px) { 
+    padding: 6rem;
+  }`;
+const TextContainer = styled.div`
+  padding: 0 4px;
+  text-align: center;
+  @media (min-width: 640px) { padding: 0; }
+`;
+
+const FormContainer = styled.form`
+  width: 100%;
+  max-width: 28rem;
+  margin-top: 2rem;
+  margin-bottom: 8px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 0.75rem;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  height: 42px;
+  outline: none;
+  border: 1px solid rgba(26, 35, 126, 0.1);
+  padding: 10px;
+  border-bottom: 1.4px solid transparent;
+  transition: all 200ms ease-in-out;
+  font-size: 12px;
+  box-shadow: 0 0 2.5px rgba(26, 35, 126, 0.85);
+  border-radius: 0.75rem;
+  margin-bottom: 1rem;
+
+  &::placeholder {
+    color: rgba(200, 200, 200, 1);
+  }
+
+  &:not(:last-of-type) {
+    border-bottom: 1.5px solid rgba(26, 35, 126, 0.1);
+  }
+
+  &:focus {
+    outline: none;
+    border-bottom: 2px solid rgb(145, 86, 235);
+  }
+`;
+export const SubmitButton = styled.button`
+  width: 100%;
+  padding: 11px 40%;
+  color: #fff;
+  font-size: 15px;
+  font-weight: 600;
+  border: none;
+  border-radius: 100px 100px 100px 100px;
+  cursor: pointer;
+  transition: all, 240ms ease-in-out;
+  background: rgb(66, 55, 193);
+  background: linear-gradient(58deg,
+  rgba(66, 35, 193, 1) 20%,
+  rgba(66, 35, 193, 1) 100%);
+
+  &:hover {
+    filter: brightness(1.03);
+  }
+`;
 
 export default function signUp() {
     return (
-        <div>
-            <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-20 sm:py-6">
-                <div className="px-4 text-center sm:px-0">
-                    <p className="text-sm font-semibold text-indigo-600 uppercase">
+            <OutsideContainer>
+                <TextContainer>
+                    <p className="text-sm font-semibold text-indigo-900 uppercase">
                         Sign up to your account
                     </p>
-                    <h2 className="mt-1 text-4xl font-extrabold text-gray-900 sm:mt-5 sm:text-6xl sm:tracking-tight lg:text-6xl">
-                        Welcome
-                    </h2>
+                  <Logo  className="text-6xl"/>
                     <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
                         Already have an account?{' '}
                         <a
-                            href="#"
+                            href="/login"
                             className="font-medium text-indigo-600 hover:text-indigo-500"
                         >
                             Sign in
                         </a>
                     </p>
-                </div>
-
-                <div className="w-full max-w-md mt-8 space-y-8">
-                    <div>
-                        <label htmlFor="email-address" className="sr-only">
-                            Email address
-                        </label>
-                        <input
-                            id="email-address"
-                            name="email-address"
-                            type="email"
-                            autoComplete="email"
-                            required
-                            className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                            placeholder="Email address"
-                        />
-                    </div>
-
-                    <div className="space-y-1">
-                        <label htmlFor="password" className="sr-only">
-                            Password
-                        </label>
-                        <input
-                            id="password"
-                            name="password"
-                            type="password"
-                            autoComplete="current-password"
-                            required
-                            className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                            placeholder="Password"
-                        />
-                    </div>
-
-                    <div className="flex items-center justify-between">
-                        <div className="flex items-center">
-                            <input
-                                id="remember-me"
-                                name="remember-me"
-                                type="checkbox"
-                                className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                            />
-                            <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-                                Remember me
-                            </label>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+                </TextContainer>
+                <FormContainer>
+                    <Input type="text" placeholder="Full Name" />
+                    <Input type="email" placeholder="Email" />
+                    <Input type="password" placeholder="Password" />
+                    <Input type="password" placeholder="Confirm Password" />
+                </FormContainer>
+                <SubmitButton type="submit">Sign up</SubmitButton>
+            </OutsideContainer>
     )
 }
-
