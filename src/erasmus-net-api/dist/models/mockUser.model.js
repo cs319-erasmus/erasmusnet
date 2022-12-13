7ff85dfaf404af85f7dfc18e7ef3c3b6de13d6d0
workbook-h@192.168.68.111 <>
2022-12-14 01:14:59 +0300

    HTTP Error Handling Done

---
+++ src/erasmus-net-api/dist/models/mockUser.model.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MockUser = void 0;
+class MockUser {
+}
+exports.MockUser = MockUser;
+//# sourceMappingURL=mockUser.model.js.map
