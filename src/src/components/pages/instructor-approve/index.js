e448c3b01598afccf766f7feb855377d7c0ab041
zeynepoztunc <zeynepsoztunc@gmail.com>
2022-12-16 01:35:18 +0300

    updated course approve/reject page
    -tables fixed: they now get values from a const array

--- src/src/components/pages/instructor-approve/index.js
+++ src/src/components/pages/instructor-approve/index.js
@@ -8,134 +8,92 @@ import { Link} from "@nextui-org/react";
 import {SSRProvider} from '@react-aria/ssr';
 
 export default function Home() {
-  // const columns = [
-  //   {
-  //     key: "name",
-  //     label: "STUDENT NAME",
-  //   },
-  //   {
-  //     key: "view",
-  //     label: "",
-  //   },
-  //   {
-  //     key: "approve",
-  //     label: "",
-  //   },
-  //   {
-  //     key: "reject",
-  //     label: "",
-  //   },
-  // ];
-  // const rows = [
-  //   {
-  //     key: "1",
-  //     name: "",
-  //     view: "",
-  //     approve: "",
-  //     reject: "",
-  //   },
-  //   {
-  //     key: "2",
-  //     name: "",
-  //     view: "",
-  //     approve: "",
-  //     reject: "",
-      
-  //   },
-  //   {
-  //     key: "3",
-  //     name: "",
-  //     view: "",
-  //     approve: "",
-  //     reject: "",
-  //   },
-  //   {
-  //     key: "4",
-  //     name: "",
-  //     view: "",
-  //     approve: "",
-  //     reject: "",
-  //   },
-  // ];
+  const columns = [
+    {
+      key: "name",
+      label: "STUDENT NAME",
+    },
+    {
+      key: "col2",
+      label: "",
+    },
+    {
+      key: "col3",
+      label: "",
+    },
+    {
+      key: "col4",
+      label: "",
+    },
+  ];
+  const rows = [
+    {
+      key: "1",
+      name: "Alperen Toprak",
+      col2: "",
+      col3: "",
+      col4: "",
 
+    },
+    {
+      key: "2",
+      name: "Eda Yılmaz",
+      col2: "",
+      col3: "",
+      col4: "",
+
+    },
+    {
+      key: "3",
+      name: "Melis Titiz",
+      col2: "",
+      col3: "",
+      col4: "",
+
+    },
+    {
+      key: "4",
+      name: "Ali Dalaslan",
+      col2: "",
+      col3: "",
+      col4: "",
+    },
+  ];
   return (
-    <div width= '100%' layout='fill'>
-     
-        <Table
+    <div width= '100%' height= '100%'layout='fill'>
+      <Table
+        aria-label="Example table with dynamic content"
         css={{
-        height: "600px",
-        width: '100%',
-        px: "$5",
-        margin:"$5",
-        layout:'fill'
-
-      }}
-    >
-      <Table.Header width="100%"  layout='fill'>
-        <Table.Column><Text css={{
-       fontSize:"15px",color:"indigo"
-      }}>STUDENT NAME</Text></Table.Column>
-        <Table.Column></Table.Column>
-        <Table.Column></Table.Column>
-        <Table.Column></Table.Column>
-      </Table.Header>
-  
-
-      <Table.Body>
-        <Table.Row width="100%" key="1">
-          <Table.Cell><Text b color="indigo" >Alperen Toprak</Text></Table.Cell>
-
-          <Table.Cell><Button color="indigo" ><Link href="/instructor-view-course" color="secondary">View</Link> </Button> </Table.Cell>
-            {/* <a href="/approvalinfo"color="white" >View</a> */}
+          height: '600px',
+          width: '100%',
+          px: "$5",
+          margin:"$5",
+          layout:'fill'
+        }}
+      >
+        <Table.Header columns={columns}>
+          {(column) => (
+            <Table.Column key={column.key}>  <Text css={{
+              fontSize:"15px",color:"indigo"
+            }}>{column.label} </Text></Table.Column>
+          )}
+        </Table.Header>
+        <Table.Body items={rows}>
+          {(item) => (
+            <Table.Row key={item.key}>
+            
+            <Table.Cell><Text  b color="indigo">{item.name}</Text></Table.Cell>
+            <Table.Cell><Button color="indigo" ><Link href="/instructor-view-course" color="secondary">View</Link> </Button> </Table.Cell>
             <Table.Cell><Button color="success">Approve</Button></Table.Cell>
-          <Table.Cell ><Button color="error" >Reject</Button></Table.Cell>
-        </Table.Row>
+            <Table.Cell ><Button color="error" >Reject</Button></Table.Cell>
+          </Table.Row>
+          )}
+        
+        </Table.Body>
+      </Table>
+  );
     
+  </div>
 
-        <Table.Row key="2">
-          <Table.Cell><Text b color="indigo">Eda Yılmaz</Text></Table.Cell>
-          <Table.Cell><Button color="indigo" ><Link href="/instructor-view-course" color="secondary">View</Link> </Button> </Table.Cell>
-          <Table.Cell><Button color="success">Approve</Button></Table.Cell>
-          <Table.Cell ><Button color="error" >Reject</Button></Table.Cell>
-        </Table.Row>
-      
-
-        <Table.Row key="3">
-          <Table.Cell><Text  b color="indigo">Melis Titiz</Text></Table.Cell>
-          <Table.Cell><Button color="indigo" ><Link href="/instructor-view-course" color="secondary">View</Link> </Button> </Table.Cell>
-          <Table.Cell><Button color="success">Approve</Button></Table.Cell>
-          <Table.Cell ><Button color="error" >Reject</Button></Table.Cell>
-        </Table.Row>
-      </Table.Body>
-    </Table>
-
-  {/* <Table
-        css={{
-        height: "600px",
-        width: '100%',
-        px: "$5",
-        margin:"$5",
-        layout:'fill'
-
-      }}
-    >
-      <Table.Header width="100%"  layout='fill' columns={columns}> {(column) => (
-        <Table.Column  key={column.key}><Text css={{
-       fontSize:"15px",color:"primary"
-      }}>{column.label}</Text></Table.Column>
-      )}</Table.Header>
-      <Table.Body items={rows}>  {(item) => (
-        <Table.Row width="100%" key={item.key}>
-          <Table.Cell><Text  b h4>{item[columnKey]}</Text></Table.Cell>
-  
-        </Table.Row>
-       )}
-      </Table.Body>
-    </Table> */}
-    
-    </div>
-  
-  
-    
   )
 }
