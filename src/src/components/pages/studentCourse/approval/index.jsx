4a3ec70fc6b11b88970d6f992ccd97749c31aa79
beyzacaglar <102811663+beyzacaglar@users.noreply.github.com>
2022-12-16 14:36:10 +0300

    Update index.jsx

--- src/src/components/pages/studentCourse/approval/index.jsx
+++ src/src/components/pages/studentCourse/approval/index.jsx
@@ -15,29 +15,21 @@ function studentCourseApproval()
     const courseItems = courses.map((course) => {
         return(
             <div>
-                <div class="grid gap-12 md:grid-cols-4">
-                    <label for="bilkentCourse" class="block mb-2 text-ml font-bold text-indigo-900">Bilkent Course</label>
-                    <label for="erasmusCourse" class="block mb-2 text-ml font-bold text-indigo-900">Erasmus Course</label>
-                    <label for="status" class="block mb-2 text-ml font-bold text-indigo-900">Status</label>
-                    <label for="firstName"></label>
-                </div>
-
                 <div class="grid gap-12 mb-6 md:grid-cols-4">
-                <button type="button" class="bg-gray-50 border border-gray-300 text-gray-700 text-ml rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" disabled>
-                    { course.bilkent }
-                </button>
-                <button type="button" class="bg-gray-50 border border-gray-300 text-gray-700 text-ml rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" disabled>
-                    { course.erasmus }
-                </button>
-                <button type="button" class="bg-gray-50 border border-gray-300 text-gray-700 text-ml rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" disabled>
-                    { course.status }
-                </button>    
-                <motion.button
-                  whileTap={{ scale: 0.9 }}
-                  class="bg-transparent border-2 font-semibold border-indigo-900 text-indigo-900 p-2 px-12 rounded-lg hover:bg-indigo-900 hover:text-white"
-                >
-                  View Request
-                </motion.button>                  
+                    <button type="button" class="bg-gray-50 border border-gray-300 text-gray-700 text-ml rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" disabled>
+                        { course.bilkent }
+                    </button>
+                    <button type="button" class="bg-gray-50 border border-gray-300 text-gray-700 text-ml rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" disabled>
+                        { course.erasmus }
+                    </button>
+                    <button type="button" class="bg-gray-50 border border-gray-300 text-gray-700 text-ml rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" disabled>
+                        { course.status }
+                    </button>    
+                    <motion.button
+                        whileTap={{ scale: 0.9 }}
+                        class="bg-transparent border-2 font-semibold border-indigo-900 text-indigo-900 p-2 px-12 rounded-lg hover:bg-indigo-900 hover:text-white">
+                    View Request
+                    </motion.button>                  
                 </div>
             </div>
         );
@@ -48,7 +40,15 @@ function studentCourseApproval()
                 <h1>Course Approval</h1>
                 <hr class="my-4 h-0.5 bg-indigo-900 border-0"></hr>
             </div>
-            <div className='m-12'>
+            <div>
+            <div class="grid gap-12 ml-3 md:grid-cols-4">
+                    <label for="bilkentCourse" class="block mb-2 text-ml font-bold text-indigo-900">Bilkent Course</label>
+                    <label for="erasmusCourse" class="block mb-2 text-ml font-bold text-indigo-900">Erasmus Course</label>
+                    <label for="status" class="block mb-2 text-ml font-bold text-indigo-900">Status</label>
+                    <label for="empty"></label>
+                </div>
+            </div>
+            <div className='m-3'>
                 { courseItems }
 
                 <div className='flex items-center justify-center'>
