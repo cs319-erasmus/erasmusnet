1bbb09b57dce8e4c730ab52c8104f191d9e83ff1
kagani <>
2022-12-17 19:29:34 +0300

    [UI] Auth Routes

--- src/src/App.js
+++ src/src/App.js
@@ -1,37 +1,59 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import { AnimatePresence } from 'framer-motion'
-import { Header, Footer,  Landing, SignUp, Login, FAQ, Appointments, Dashboard, Profile, Placement, Error, StudentCourseApproval, StudentCourseRequest,CoordinatorApproval, NavigationMenu } from './components'
-import { AuthProvider } from './contexts/AuthProvider'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import {
+  Header,
+  Footer,
+  Landing,
+  SignUp,
+  Login,
+  FAQ,
+  Appointments,
+  Dashboard,
+  Profile,
+  Placement,
+  Error,
+  StudentCourseApproval,
+  StudentCourseRequest,
+  CoordinatorApproval,
+  NavigationMenu,
+} from "./components";
+import { AuthProvider } from "./contexts/AuthProvider";
+import { UserRoutes } from "./utils/UserRoutes"
 
 const App = () => {
   return (
     <AnimatePresence>
-      <div className='w-screen m-auto flex flex-col'>
+      <div className="w-screen m-auto flex flex-col">
         <AuthProvider>
-        <Header />
-        <NavigationMenu />
-        <main className='py-8 px-1 lg:p-8 lg:px-0'>
-          <Routes>
-            <Route path='/' element={<Landing />} />
-            <Route path='/signup' element={<SignUp />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/faq' element={<FAQ />} />
-            <Route path='/appointments' element={<Appointments />} />
-            <Route path='/dashboard' element={<Dashboard />} />
-            <Route path='/profile' element={<Profile />} />
-            <Route path='/placement' element={<Placement />} />
-            <Route path='/studentCourseApproval' element={<StudentCourseApproval />} />
-            <Route path="/request" element={<StudentCourseRequest />} />
-            <Route path="/request" element={<CoordinatorApproval />} />
-            <Route path='*' element={<Error />} />
-          </Routes>
-        </main>
-        <Footer />
+          <Header />
+          <main className="px-1 lg:px-0">
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="*" element={<Error />} />
+
+              <Route element={<UserRoutes />}>
+                <Route path="/faq" element={<FAQ />} />
+                <Route path="/appointments" element={<Appointments />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/placement" element={<Placement />} />
+                <Route
+                  path="/studentCourseApproval"
+                  element={<StudentCourseApproval />}
+                />
+                <Route path="/request" element={<StudentCourseRequest />} />
+
+              </Route>
+            </Routes>
+          </main>
+          <Footer />
         </AuthProvider>
       </div>
     </AnimatePresence>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/src/components/pages/login/index.jsx
+++ src/src/components/pages/login/index.jsx
@@ -13,6 +13,11 @@ export default function Login() {
   const { user } = useAuth();
 
   const navigate = useNavigate();
+
+  if (user !== null) {
+    navigate("/dashboard", { replace: true });
+  }
+  
   const signup = () => {
     navigate("/signup", { replace: true });
   };
@@ -35,7 +40,7 @@ export default function Login() {
   };
 
   return (
-    <div className="flex flex-col items-center max-w-screen-xl m-auto">
+    <div className="flex flex-col items-center mt-8 max-w-screen-xl m-auto">
       <Logo className="text-3xl" />
       <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
         Don't have an account?{" "}
