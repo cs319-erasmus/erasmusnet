b85da6ad27f3ed493c376e4b0d18031e570c31ce
zeynepoztunc <zeynepsoztunc@gmail.com>
2022-12-18 14:34:07 +0300

    added routing for student-view-request page

--- src/src/App.js
+++ src/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
 
-import { Header, Footer,  Landing, SignUp, Login, FAQ, Appointments, Dashboard, Profile, Placement, Error } from './components'
+import { Header, Footer,  Landing, SignUp, Login, FAQ, Appointments, Dashboard, Profile, Placement, Error,StudentViewRequest } from './components'
 
 
 const App = () => {
@@ -21,6 +21,7 @@ const App = () => {
             <Route path='/dashboard' element={<Dashboard />} />
             <Route path='/profile' element={<Profile />} />
             <Route path='/placement' element={<Placement />} />
+            <Route path='/student-view-request' element={<StudentViewRequest />} />
             <Route path='*' element={<Error />} />
           </Routes>
         </main>
