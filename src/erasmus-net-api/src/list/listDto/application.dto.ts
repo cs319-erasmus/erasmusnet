6c814fca68f52270cf7ecc46c5d9fe66ceb6e171
workbook-h@Workbooks-MacBook-Pro.local <>
2022-12-19 04:54:30 +0300

    not done 2

---
+++ src/erasmus-net-api/src/list/listDto/application.dto.ts
@@ -0,0 +1,10 @@
+export class ApplicationDTO {
+    uid: string;
+    schoolChoices: string[];
+    studentName: string;
+    totalPoints: string;
+    department: string;
+    studentId: string;
+    exchangSemester: string;
+    placementRef: string;
+}
---
+++ src/erasmus-net-api/src/list/listDto/placedStudentsList.dto.ts
@@ -0,0 +1,6 @@
+import { ApplicationDTO } from "./application.dto";
+
+export class PlacedStudentsListDTO {
+    isStudentsPlaced: boolean;
+    applications: ApplicationDTO[];
+}
