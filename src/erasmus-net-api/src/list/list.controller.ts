21c804835e634e1d03fff6ea4c6f3550814e59b8
Arda Baktir <arda.baktir@ug.bilkent.edu.tr>
2022-12-23 01:09:43 +0300

    excel upload

--- src/erasmus-net-api/src/list/list.controller.ts
+++ src/erasmus-net-api/src/list/list.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile, Headers, ParseFilePipe, FileTypeValidator } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express/multer';
 import { ListService } from './list.service';
 import { PlacedStudentsListDTO } from './listDto/placedStudentsList.dto';
 import { WaitingListDTO } from './listDto/waitinglist.dto';
+import admin from 'src/main';
+import * as XLSX from 'xlsx';
+import { diskStorage } from 'multer';
+import * as csvToJson from 'convert-csv-to-json';
+
 
 @Controller('api/list')
 export class ListController {
@@ -12,6 +18,26 @@ export class ListController {
   @Body('placedstudentslist') placedStudentsListDTO: PlacedStudentsListDTO) {
     return this.listService.create(waitingListDTO, placedStudentsListDTO);
   }
+  @Post("excel")
+  @UseInterceptors(FileInterceptor('file',{
+    storage: diskStorage({
+      destination: './uploads'
+    })
+  }))
+  uploadExcel(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new FileTypeValidator({ fileType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' }),
+        ],
+      }),
+    )file: Express.Multer.File) {
+      const workBook = XLSX.readFile("./uploads/"+file.filename);
+      XLSX.writeFile(workBook, "./uploads/excel.csv", { bookType: "csv" });
+      let json = csvToJson.getJsonFromCsv("./uploads/excel.csv");
+      admin.firestore().collection("lists").doc("lists").set(json);
+      
+    }
 
   @Get()
   findOne() {
