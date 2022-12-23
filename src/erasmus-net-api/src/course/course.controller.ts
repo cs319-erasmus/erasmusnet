cf8ffc4c399ed1312dfba55e5900a00226a3f3b6
Arda Baktir <arda.baktir@ug.bilkent.edu.tr>
2022-12-23 06:33:03 +0300

    syllabus uploads

--- src/erasmus-net-api/src/course/course.controller.ts
+++ src/erasmus-net-api/src/course/course.controller.ts
@@ -20,34 +20,51 @@ export class CourseController {
   }
 
   @Post('syllabus')
-    @UseInterceptors(
-      FileInterceptor('file', {
-        storage: diskStorage({
-          destination: './uploads',
-        }),
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+      }),
+    }),
+  )
+  async uploadExcel(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new FileTypeValidator({
+            fileType: 'application/pdf',
+          }),
+        ],
       }),
     )
-    async uploadExcel(
-      @UploadedFile(
-        new ParseFilePipe({
-          validators: [
-            new FileTypeValidator({
-              fileType:
-                'application/pdf',
-            }),
-          ],
-        }),
-      )
-      file: Express.Multer.File,
+    file: Express.Multer.File,
     @Headers('uid') uid: string,
-    @Headers('approvalId') approvalId: string){
-      //upload file to firestore
+    @Headers('approvalId') approvalId: string
+  ) {
+
+
+    const storage = admin.storage();
+
+    try {
+      const signedUrlResponse: string[] = await storage
+        .bucket("erasmus-net-e2c1c.appspot.com")
+        .file("./uploads/${file.filename}")
+        .getSignedUrl({
+          action: 'read',
+          expires: new Date(Date.now() + 10000 * 60000),
+        });
+
+      const assetUrl: string = signedUrlResponse[0];
+      var obj = {};
+
       admin.firestore().collection('courses').doc(uid).update({
-        [approvalId]: { syllabus: file.buffer}
-      });
-      return true;
+        syllabus: assetUrl
+      }) 
+      return assetUrl;
+    } catch (error) {
+      console.log(error);
     }
-          
+  }
 
   //gets a courseLink by its uid
   @Get()
